5b2b20eb9df7181963e73f82122d0cdb12c84b02
unknown <mdsakiluzzaman@gmail.com>
2022-10-03 22:28:57 +0600

    some changes

--- server1.js
+++ server1.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to bezkoder application." });
+    res.json({ message: "Welcome to E-commerec application." });
 });
 
 // set port, listen for requests
