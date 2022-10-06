ded36693d66cadba835bb89241258cf44ee4b3f5
Sakil <mdsakiluzzaman@gmail.com>
2022-10-06 15:43:18 +0600

    dev msg changed

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cors(corsOptions));
 
 // simple route
 app.get("/", (req, res) => {
-    res.json({ message: "Hurrah !!!!!!!!!! Have fun!!" });
+    res.json({ message: "Dev Merge Test Working!!!!!!!" });
 });
 
 // set port, listen for requests
