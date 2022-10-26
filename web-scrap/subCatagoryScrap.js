4c55c6d3dac8c440b0c03c0a8ee3bf503b16afdc
unknown <mdsakiluzzaman@gmail.com>
2022-10-26 23:28:16 +0600

    some changes

--- web-scrap/subCatagoryScrap.js
+++ web-scrap/subCatagoryScrap.js
@@ -12,13 +12,17 @@ const getPostTitles = async() => {
         const postTitles = [];
         // return "shauwa"
         // return $('div > p.web-header-address > span.web-header-address-title').text();
-        $('div > div.imageWrapper').each((_idx, el) => {
-            console.log(el)
-                // const postTitle = $(el).text();
-                // const image = $(el).attr().src;
-                // const postTitle = $(el).find('.name').text();
+        $('a.category > div ').each((_idx, el) => {
+            // console.log(el)
+            // const postTitle = $(el).text();
+            // const image = $(el).attr().src;
+            const postTitle = $(el).find('.name').text();
             const image = $(el).find('img').attr().src;
-            postTitles.push(image)
+            let obj = {
+                title: postTitle,
+                image: image
+            }
+            postTitles.push(obj)
         });
 
         return postTitles;
