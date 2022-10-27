0550dbff3956b528a4f7f7f198968ee66a6c2e3d
sakilbd <mdsakiluzzaman.com>
2022-10-27 13:37:33 +0600

    simplified puppetier scroll test

--- test.js
+++ test.js
@@ -1,11 +1,34 @@
 const puppeteer = require('puppeteer');
 
 // starting Puppeteer
+
+
+async function autoScroll(page) {
+    await page.evaluate(async() => {
+        await new Promise((resolve) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if (totalHeight >= scrollHeight - window.innerHeight) {
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        });
+    });
+}
 puppeteer.launch().then(async() => {
 
     // opening a new page and navigating to Reddit
     const browser = await puppeteer.launch({
-        headless: false
+        headless: false,
+        slowMo: 250,
+
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-notifications', '--incognito'], // these are used to disable notification 
     });
 
     // Create a new page
@@ -13,6 +36,26 @@ puppeteer.launch().then(async() => {
 
     // Configure the navigation timeout
     await page.setDefaultNavigationTimeout(0);
+
+
+    //this function used to disable location popup 
+    await page.evaluateOnNewDocument(function() {
+        navigator.geolocation.getCurrentPosition = function(cb) {
+            setTimeout(() => {
+                cb({
+                    'coords': {
+                        accuracy: 21,
+                        altitude: null,
+                        altitudeAccuracy: null,
+                        heading: null,
+                        latitude: 23.129163,
+                        longitude: 113.264435,
+                        speed: null
+                    }
+                })
+            }, 3000)
+        }
+    });
     await page.goto('https://chaldal.com/fresh-vegetable');
     await autoScroll(page);
     await page.waitForSelector('body');
