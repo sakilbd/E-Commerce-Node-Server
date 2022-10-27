bc1dfcac1c83056924406a6a636b8b6abe28ba7d
sakilbd <mdsakiluzzaman.com>
2022-10-27 18:00:14 +0600

    some updates with scrap

--- web-scrap/cheerio/subCatagoryScrap.js
+++ web-scrap/cheerio/subCatagoryScrap.js
@@ -1,28 +1,30 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const cheerio = require('cheerio');
-
+const cheerio = require("cheerio");
+const fs = require("fs");
+var jsonFileName;
 const getPostTitles = async() => {
     try {
-        const { data } = await axios.get(
-            'https://chaldal.com/food'
-        );
+        let url = "https://chaldal.com/cleaning"
+        let urlSplit = url.split('/');
+        jsonFileName = urlSplit[urlSplit.length - 1];
+        const { data } = await axios.get(url);
         const $ = cheerio.load(data);
 
         const postTitles = [];
         // return "shauwa"
         // return $('div > p.web-header-address > span.web-header-address-title').text();
-        $('a.category > div ').each((_idx, el) => {
+        $("a.category > div ").each((_idx, el) => {
             // console.log(el)
             // const postTitle = $(el).text();
             // const image = $(el).attr().src;
-            const postTitle = $(el).find('.name').text();
-            const image = $(el).find('img').attr().src;
+            const postTitle = $(el).find(".name").text();
+            const image = $(el).find("img").attr().src;
             let obj = {
                 title: postTitle,
-                image: image
-            }
-            postTitles.push(obj)
+                image: image,
+            };
+            postTitles.push(obj);
         });
 
         return postTitles;
@@ -31,5 +33,10 @@ const getPostTitles = async() => {
     }
 };
 
-getPostTitles()
-    .then((postTitles) => console.log(postTitles));
+getPostTitles().then((postTitles) => {
+    fs.writeFile(`./web-scrap/cheerio/sub-catagory/${jsonFileName}.json`, JSON.stringify(postTitles), (err) => {
+        if (err) throw err;
+        console.log('Data written to file');
+    });
+    console.log(postTitles);
+});
