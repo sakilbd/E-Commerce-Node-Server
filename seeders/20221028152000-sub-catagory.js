cfbdc999939fcda0dbdd3ae669e6c8231db9ccc2
unknown <mdsakiluzzaman@gmail.com>
2022-10-28 22:38:43 +0600

    some progress done

--- seeders/20221028152000-sub-catagory.js
+++ seeders/20221028152000-sub-catagory.js
@@ -1,24 +1,26 @@
-'use strict';
-let data = require('../web-scrap/scraped-data/sub-catagory/food.json');
+"use strict";
+let data = require("../web-scrap/scraped-data/sub-catagory/food.json");
+let download = require("../web-scrap/image-download.js")
 
+download.downloadImage();
 let item = [];
-for (let i = 0; i < data.length; i++) {
+for (let dt of data) {
     item.push({
-        sub_catagory_title: 'John' + i,
-        image: 'Doe' + i,
+        root_catagory_id: 1,
+        sub_catagory_title: dt.title,
+        image: dt.image,
 
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
     });
 }
 
-
 module.exports = {
     async up(queryInterface, Sequelize) {
-        return queryInterface.bulkInsert('sub_catagories', item);
+        return queryInterface.bulkInsert("sub_catagories", item);
     },
 
     async down(queryInterface, Sequelize) {
-        return queryInterface.bulkDelete('sub_catagories', null, {});
-    }
+        return queryInterface.bulkDelete("sub_catagories", null, {});
+    },
 };
