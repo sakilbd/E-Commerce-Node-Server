56afbecbaa5d8cf96a0522188f61527d309eebc8
unknown <mdsakiluzzaman@gmail.com>
2022-10-29 18:51:27 +0600

    some changes

--- seeders/20221028152019-child-catagory.js
+++ seeders/20221028152019-child-catagory.js
@@ -1,12 +1,31 @@
-'use strict';
+"use strict";
 
-/** @type {import('sequelize-cli').Migration} */
+let data = require("../web-scrap/data-to-seed/sub-catagory-to-seed.json");
+
+
+const item = [
+
+];
+// for (let dt of data) {
+
+data.forEach(data => {
+    item.push({
+        sub_catagory_id: 1,
+        child_catagory_title: data.title,
+        image: data.image,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    });
+})
+
+// }
+
 module.exports = {
     async up(queryInterface, Sequelize) {
-
+        return queryInterface.bulkInsert("child_catagories", item);
     },
 
     async down(queryInterface, Sequelize) {
-
-    }
+        return queryInterface.bulkDelete("child_catagories", null, {});
+    },
 };
