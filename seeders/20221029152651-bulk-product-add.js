3e8aa1882019083c927da397e7852612a271430a
sakilbd <mdsakiluzzaman.com>
2022-10-30 11:05:09 +0600

    some updates

--- seeders/20221029152651-bulk-product-add.js
+++ seeders/20221029152651-bulk-product-add.js
@@ -10,13 +10,17 @@ const item = [
 // for (let dt of data) {
 
 data.forEach(data => {
+
+
+
     item.push({
         child_catagory_id: process.env.CHILD_CATAGORY_ID,
         product_title: data.title,
         image: data.image,
         quantity: data.quantity,
-        discounted_price: data.discounted_price,
-        price: data.price,
+        discounted_price: (data.discounted_price).split(',').join(''),
+        price: (data.price).split(',').join(''),
+        rating: ((Math.random() * (5 - 3)) + 3).toFixed(2),
         createdAt: new Date(),
         updatedAt: new Date(),
     });
---
+++ test.js
@@ -0,0 +1,6 @@
+const price = "1,124,1,2,3,"
+
+console.log(((Math.random() * (5 - 3)) + 3).toFixed(2))
+
+
+console.log(price.split(",").join(''))
